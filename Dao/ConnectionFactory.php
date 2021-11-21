d0d4f33dddb89f57584aad4ca38787bc11c3a70c
Kaio Ruan <Kaio.martins2@aluno.unip.br>
2021-11-21 20:07:45 -0300

    CRuD check

--- Dao/ConnectionFactory.php
+++ Dao/ConnectionFactory.php
@@ -2,7 +2,7 @@
 class ConnectionFactory{
     public static function getConnection(){  
         try {             
-            return $db = new PDO('pgsql:host=localhost;dbname=CRUD', "mtskaioken", "WilsonAtacante123@");
+            return $db = new PDO('pgsql:host=localhost;dbname=CRUD', "USER", "SENHA");
 
         } catch (PDOException $e) {
             echo 'Exceção capturada: ',  $e->getMessage(), "\n";
